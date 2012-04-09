20ec152fd4dbd53c9abe268c330f6bc66c1a4508
OmniBus <omnibus.syli@gmail.com>
2012-04-09 17:44:21 +0800

    [DEPRECATION] `insert_after` hook method is deprecated, replace hook call

--- lib/multi_currencies_hooks.rb
+++ lib/multi_currencies_hooks.rb
@@ -1,18 +1,20 @@
 class MultiCurrenciesHooks < Spree::ThemeSupport::HookListener
   # custom hooks go here
-  insert_after :admin_configurations_menu do
-    %(
+  Deface::Override.new(:virtual_path => "admin/configurations/index",
+                       :name => "converted_admin_configurations_menu_626666908",
+                       :insert_after => "[data-hook='admin_configurations_menu'], #admin_configurations_menu[data-hook]",
+                       :text => "
       <% if current_user.has_role?(:admin) %>
        <tr>
-         <td><%= link_to t("currency_settings"), admin_currencies_path %></td>
-         <td><%= t("currency_description") %></td>
+         <td><%= link_to t(\"currency_settings\"), admin_currencies_path %></td>
+         <td><%= t(\"currency_description\") %></td>
        </tr>
        <tr>
-         <td><%= link_to t("currency_converters_settings"), admin_currency_converters_path %></td>
-         <td><%= t("currency_converters_description") %></td>
+         <td><%= link_to t(\"currency_converters_settings\"), admin_currency_converters_path %></td>
+         <td><%= t(\"currency_converters_description\") %></td>
        </tr>
 
       <% end %>
-     )
-  end
+     ",
+                       :disabled => false)
 end
